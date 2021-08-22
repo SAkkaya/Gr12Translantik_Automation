e63f348db1fc53ac6a634ed90e06022dd9723d9e
haciarpaci <haciarpaci12@gmail.com>
2021-08-22 18:28:31 +0300

    extended

--- src/test/java/com/translantik/stepDefs/addEventsStep.java
+++ src/test/java/com/translantik/stepDefs/addEventsStep.java
@@ -35,7 +35,7 @@ public class addEventsStep {
 
         BrowserUtils.waitForVisibility(new GeneralInformationPage().titleGenInforPage,3);
         Assert.assertEquals(expectedTitle, new GeneralInformationPage().titleGenInforPage.getText());
-
+        // added
 
     }
 
