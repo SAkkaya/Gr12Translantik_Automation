3673d77661377d66a89e479b21f14cd89d1a9d29
Galip İrice <galipirice@gmail.com>
2021-09-01 00:07:16 +0300

    This is completely f'd up

--- src/test/java/com/translantik/stepDefs/AllCarsStepDefs.java
+++ src/test/java/com/translantik/stepDefs/AllCarsStepDefs.java
@@ -1,27 +1,32 @@
 package com.translantik.stepDefs;
 
 
+import com.translantik.pages.BasePage;
 import com.translantik.pages.DashboardPage;
 import com.translantik.pages.FleetVehiclePage;
 import com.translantik.utilities.BrowserUtils;
 import com.translantik.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
+import java.util.List;
 
-
-public class AllCarsStepDefs {
-//    @When("the user navigates to {string} {string}")
-//    public void the_user_navigates_to(String tab, String module) {
-//        BrowserUtils.waitFor(7);
-//        new DashboardPage().navigateToModule(tab,module);
-//        BrowserUtils.waitFor(5);
-//    }
+public class AllCarsStepDefs  {
+    @When("the user navigates to {string} {string}")
+    public void the_user_navigates_to(String tab, String module) {
+        BrowserUtils.waitFor(7);
+        new DashboardPage().navigateToModule(tab,module);
+        BrowserUtils.waitFor(5);
+    }
 
     @Then("User should see the table  of vehicle information")
     public void user_should_see_the_table_of_vehicle_information() {
@@ -79,6 +84,93 @@ public class AllCarsStepDefs {
     }
 
     @Then("the user select the {string} {string} filtering type")
+    @When("User hovers over on ... at the end of each row in the table")
+    public void user_hovers_over_on_at_the_end_of_each_row_in_the_table() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+
+        Actions actions = new Actions(Driver.get());
+        actions.moveToElement(fleetVehiclePage.threeDot).perform();
+    }
+
+    @Then("user should see the delete option in the opened menu")
+    public void user_should_see_the_delete_option_in_the_opened_menu() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        Assert.assertTrue(fleetVehiclePage.deleteButton.isDisplayed());
+    }
+
+    @When("User clicks on delete vehicle button")
+    public void user_clicks_on_delete_vehicle_button() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        fleetVehiclePage.deleteButton.click();
+
+
+
+    }
+    @When("User confirms delete by clicking yes")
+    public void User_confirms_delete_by_clicking_yes() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        fleetVehiclePage.confirmDelete.click();
+
+
+    }
+
+
+    @Then("no permission message should be displayed")
+    public void no_permission_message_should_be_displayed() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        String actualMessage = fleetVehiclePage.confirmationMessage.getText();
+        String expectedMessage = "You do not have permission to perform this action.";
+        Assert.assertEquals(expectedMessage,actualMessage);
+    }
+
+    @When("the user clicks on one of the cars in the table")
+    public void the_user_clicks_on_one_of_the_cars_in_the_table() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        fleetVehiclePage.firstCarOnTable.click();
+
+    }
+
+    @Then("the user should not be able to see delete button on the page")
+    public void the_user_should_not_be_able_to_see_delete_button_on_the_page() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        Assert.assertFalse(fleetVehiclePage.removeButton.isDisplayed());
+
+    }
+    @Then("the user should be able to see delete button on the page")
+    public void the_user_should_be_able_to_see_delete_button_on_the_page() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        Assert.assertTrue(fleetVehiclePage.removeButton.isDisplayed());
+    }
+    @Then("delete confirmation pop up should be displayed")
+    public void delete_confirmation_pop_up_should_be_displayed() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        Assert.assertTrue(fleetVehiclePage.confirmDelete.isDisplayed());
+    }
+    @Then("car is succesfully deleted and deleted message is displayed")
+    public void car_is_succesfully_deleted_and_deleted_message_is_displayed() {
+        FleetVehiclePage fleetVehiclePage = new FleetVehiclePage();
+        String actualMessage = fleetVehiclePage.confirmationMessage.getText();
+        String expectedMessage = "Item deleted";
+        Assert.assertEquals(expectedMessage,actualMessage);
+
+
+    }
+
+
+  /*  @Then("the user select the {string} filtering type")
+    public void theUserSelectTheFilteringType(String string) {
+        FleetVehiclePage fleetVehiclePage=new FleetVehiclePage();
+        BrowserUtils.waitForPageToLoad(10);
+        BrowserUtils.waitForClickablility(fleetVehiclePage.filterButton,10);
+        fleetVehiclePage.filterButton.click();
+        BrowserUtils.waitForVisibility(fleetVehiclePage.manageFilters,10);
+        fleetVehiclePage.manageFilters.click();
+        BrowserUtils.waitForVisibility(fleetVehiclePage.manageFilterSearchBox,10);
+        fleetVehiclePage.methodFilter(string);
+
+    }*/
+
+    @Then("the user select the {string}, {string} filtering type")
     public void theUserSelectTheFilteringType(String arg0, String arg1) {
         new FleetVehiclePage().selectFilterType(arg0, arg1);
     }
---
+++ src/test/resources/features/TRANS533.feature
@@ -0,0 +1,71 @@
+
+Feature:
+
+	#AC 1 - All users can see the delete option by hovering over ... on the table at the end of each row
+	@TRANS-587
+	Scenario Outline: All users can see the delete option by hovering over
+		            Given the user logged in as "<userType>"
+				    And the user navigates to "Fleet" "Vehicles"
+				    When User hovers over on ... at the end of each row in the table
+				    Then user should see the delete option in the opened menu
+				    Examples:
+				      | userType      |
+				      | driver        |
+				      | store manager |
+				      | sales manager |
+  	#2-"You do not have permission to perform this action." message should be displayed if the driver attempts to delete a car.
+	@TRANS-588
+	Scenario: The driver attempts to delete a car
+		Given the user logged in as "driver"
+		And the user navigates to "Fleet" "Vehicles"
+		When User hovers over on ... at the end of each row in the table
+		And User clicks on delete vehicle button
+		And User confirms delete by clicking yes
+		Then no permission message should be displayed
+
+		#3- AC3- Driver can NOT see "delete" button on "General Information" page
+	@TRANS-589
+	Scenario: Verify that the driver should not see delete button on General Information page
+		Given the user logged in as "driver"
+		And the user navigates to "Fleet" "Vehicles"
+		When the user clicks on one of the cars in the table
+		Then the user should not be able to see delete button on the page
+
+		#AC4- Sales manager and store manager can reach the delete button on the general information page
+	@TRANS-590
+	Scenario Outline: Verify that sales manager and store manager can reach the delete button on the general information page
+		Given the user logged in as "<userType>"
+		And the user navigates to "Fleet" "Vehicles"
+		When the user clicks on one of the cars in the table
+		Then the user should be able to see delete button on the page
+		Examples:
+			| userType      |
+			| store manager |
+			| sales manager |
+
+			#AC5- "Delete Confirmation" pop up should be displayed when the user clicks on the delete button
+	@TRANS-591
+	Scenario Outline: Verify that Delete Confirmation pop up should be displayed when the user clicks on the delete button
+		Given the user logged in as "<userType>"
+		And the user navigates to "Fleet" "Vehicles"
+		And the user clicks on one of the cars in the table
+		When User clicks on delete vehicle button
+		Then delete confirmation pop up should be displayed
+		Examples:
+			| userType      |
+			| store manager |
+			| sales manager |
+
+			#AC6- After deleting a car, "Car deleted" message should be displayed and the corresponding vehicle should be removed from the table
+	@TRANS-592
+	Scenario Outline: Verify that car is successfully deleted and deleted message is displayed
+		Given the user logged in as "<userType>"
+		And the user navigates to "Fleet" "Vehicles"
+		And User hovers over on ... at the end of each row in the table
+		When User clicks on delete vehicle button
+		And User confirms delete by clicking yes
+		Then car is succesfully deleted and deleted message is displayed
+		Examples:
+			| userType      |
+			| store manager |
+			| sales manager |
